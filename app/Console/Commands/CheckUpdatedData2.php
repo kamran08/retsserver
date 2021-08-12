602b7bfa561620155d1e0562db217d9d1afb07c3
kamran08 <ahmedkamran265@gmail.com>
2021-08-12 12:56:24 +0600

    'ra2updated'

---
+++ app/Console/Commands/CheckUpdatedData2.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Http\Controllers\RetsController;
+use Illuminate\Console\Command;
+
+class CheckUpdatedData2 extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:CheckUpdatedData2';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Check updated RA_2 data from rets server and update them';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        \Log::info("Calling CheckUpdatedData");
+        $ob = new RetsController();
+        return  $ob->checkForUpdatedData2();
+    }
+}
