9e7670077b4260736819d2edeaa0cbc15faab856
kamran08 <ahmedkamran265@gmail.com>
2021-08-12 14:35:19 +0600

    'upded'

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,6 @@
 Route::post('/storeDataFromDataServer', 'DataController@storeDataFromDataServer');
 Route::post('/storeImageDataFromDataServer', 'DataController@storeImageDataFromDataServer');
 Route::get('/getData', 'DataController@getData');
-Route::get('/checkForUpdatedData2', 'RetsController@checkForUpdatedData2');
 Route::get('/getLocation', 'RetsController@getLocation');
 Route::get('/storeImages', 'RetsController@storeImages');
 Route::get('/getDatadirectoryCheck', 'DataController@directoryCheck');
