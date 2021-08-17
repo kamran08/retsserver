4a023ca134c488c6984a00eb81c5aa433253a0cc
kamran08 <ahmedkamran265@gmail.com>
2021-08-17 11:14:52 +0600

    updated

--- app/Http/Controllers/RetsController.php
+++ app/Http/Controllers/RetsController.php
@@ -227,7 +227,7 @@ class RetsController extends Controller
         }
         foreach($alldata as $key => $d){
             \Log::info("alldata");
-            if($mapreq['counter'] >=45000) return 1;
+            if($mapreq['counter'] >=25000) return 1;
             if($d['listingAddress']){
                 $d['listingAddress'] = trim($d['listingAddress'],"#");
             
