90c1cc10ba51c3f70b1f764f6e4f2ccfe8c38c2d
kamran08 <ahmedkamran265@gmail.com>
2021-09-30 17:28:22 +0600

    ups

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -35,14 +35,14 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('command:FetchAndStore')
-                 ->everyFiveMinutes();
-        $schedule->command('command:FeatchAndSrore2')
-                 ->everyFiveMinutes();
-        $schedule->command('command:latLangFetach')
-                 ->everyMinute();
-        $schedule->command('command:imageResizeAndStore')
-                 ->everyFiveMinutes();
+        // $schedule->command('command:FetchAndStore')
+        //          ->everyFiveMinutes();
+        // $schedule->command('command:FeatchAndSrore2')
+        //          ->everyFiveMinutes();
+        // $schedule->command('command:latLangFetach')
+        //          ->everyMinute();
+        // $schedule->command('command:imageResizeAndStore')
+        //          ->everyFiveMinutes();
                 //  update 
         // $schedule->command('command:updateRaData')->everyFiveMinutes();
         // $schedule->command('command:updateRaImageData')->everyFiveMinutes();
