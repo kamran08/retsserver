55e650cc5b398c010c18e21300267fa18415f2e5
kamran08 <ahmedkamran265@gmail.com>
2021-10-11 18:28:10 +0600

    updated

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -53,8 +53,8 @@ class Kernel extends ConsoleKernel
         //          ->everyMinute();  
 
         //  update 
-        // $schedule->command('command:updateRaData')->everyFiveMinutes();
-        // $schedule->command('command:updateRdData')->everyFiveMinutes();
+        $schedule->command('command:updateRaData')->everyFiveMinutes();
+        $schedule->command('command:updateRdData')->everyFiveMinutes();
 
 
         // $schedule->command('command:updateRaImageData')->everyFiveMinutes();
